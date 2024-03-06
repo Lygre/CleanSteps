e63ee542cfa50af0b4e123bb3514dc9a14fa74ad
Hugh Dylan Broome <hughdbroome@icloud.com>
2024-03-06 01:50:00 -0500

    Initial Commit

---
+++ CleanSteps/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CleanSteps
+//
+//  Created by Hugh on 3/6/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
